96614ac5c863970fd995539212a98223b80a4a98
VISHALKRBIBHUTI <126374785+VISHALKRBIBHUTI@users.noreply.github.com>
2023-05-12 22:47:22 +0530

    Update hollowrec.cpp

--- hollowrec.cpp
+++ hollowrec.cpp
@@ -6,7 +6,7 @@ int main(){
     cin>>row>>column;
     for(int i=1;i<=row;i++){
         for(int j=1;j<=column;j++){
-            if(i==1||j==1||i==row||j==column){  // for less input we can equate rows=column;
+            if(i==1||j==1||i==row||j==column){  
                 cout<<j;
             }
         
